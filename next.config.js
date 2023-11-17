cb19415fa0bf96fc0e60db190f703f615dc76af3
Karl J. Overå <k@rl.run>
2023-11-17 16:45:04 +0100

    disable minification

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    webpack(webpackConfig) {
+        return {
+            ...webpackConfig,
+            optimization: {
+                minimize: false,
+            },
+        }
+    },
     experimental: {
         serverComponentsExternalPackages: ['@navikt/next-logger', 'next-logger'],
         typedRoutes: true,
