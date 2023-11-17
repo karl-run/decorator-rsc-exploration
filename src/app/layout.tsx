52f73a81534ff4aedc2454c7c005dab061172890
Karl J. Overå <k@rl.run>
2023-11-17 15:59:41 +0100

    play around with og image generation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,19 @@ import PageLinks from '@/components/page-links'
 export const metadata: Metadata = {
     title: 'Decorator CSC Exploration',
     description: 'A workspace for testing out the decorator in Server Components',
+    openGraph: {
+        title: 'Decorator CSC Exploration',
+        description: 'A workspace for testing out the decorator in Server Components',
+        images: [
+            {
+                width: 1200,
+                height: 630,
+                url: '/og',
+            },
+        ],
+        locale: 'no_NO',
+        type: 'website',
+    },
 }
 
 export default function RootLayout({ children }: PropsWithChildren): ReactElement {
